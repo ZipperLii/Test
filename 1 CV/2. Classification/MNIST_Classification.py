8068bd1d905d2f011c9088200db9d86efc32a8a4
ZipperLi <ZipperLii@users.noreply.github.com>
2024-12-30 02:40:55 +0800

    Day6

--- 1 CV/2. Classification/MNIST_Classification.py
+++ 1 CV/2. Classification/MNIST_Classification.py
@@ -1,30 +1,33 @@
 import torch
-import torch.nn as nn
 import torchvision
-from torch.nn import functional as F
+import torch.nn as nn
 from ImageNet import LeNet
-from d2l import torch as d2l
 from torch.utils import data
 from torchvision import transforms
 from tqdm import tqdm
-    
+import matplotlib.pyplot as plt
+
 def try_gpu(i=0):
     if torch.cuda.device_count() >= i + 1:
         return torch.device(f'cuda:{i}')
     return torch.device('cpu')
 
 def get_optimizer(net, lr):
-    optimizer = torch.optim.SGD(net.parameters(), lr=lr, momentum=0.9)
+    optimizer = torch.optim.Adam(net.parameters(), lr=lr)
     return optimizer
 
-def evaluate_accuracy_gpu(net, data_iter, device=None):
-    """使用GPU计算模型在数据集上的精度"""
+def cal_correct_num(y_hat, y):
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(axis=1)
+    cmp = y_hat.type(y.dtype) == y
+    return float(cmp.type(y.dtype).sum())
+
+def cal_accuracy_gpu(net, data_iter, device=None):
     if isinstance(net, nn.Module):
-        net.eval()  # 设置为评估模式
+        net.eval()
         if not device:
             device = next(iter(net.parameters())).device
-    # 正确预测的数量，总预测的数量
-    metric = d2l.Accumulator(2)
+    metric = [0.0] * 3
     with torch.no_grad():
         for X, y in data_iter:
             if isinstance(X, list):
@@ -32,7 +35,8 @@ def evaluate_accuracy_gpu(net, data_iter, device=None):
             else:
                 X = X.to(device)
             y = y.to(device)
-            metric.add(d2l.accuracy(net(X), y), y.numel())
+            correct_list = [cal_correct_num(net(X), y), y.numel()]
+            metric = [a + float(b) for a, b in zip(metric, correct_list)]
     return metric[0] / metric[1]
 
 def save_model(net, path):
@@ -42,7 +46,37 @@ def init_weights(m):
     if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
         nn.init.xavier_uniform_(m.weight)
 
-def train_model(net, train_iter, test_iter, num_epochs, lr, device, plot=False):
+def plot_curves(epoch_train_loss, epoch_train_acc, epoch_test_acc, path='./img/Fig.jpg'):
+
+    num_epochs = len(epoch_test_acc)
+    batch_size = len(epoch_train_acc) / num_epochs
+
+    batch_indices = [i / batch_size for i in range(len(epoch_train_loss))]
+    epoch_indices = range(1, num_epochs + 1)
+
+    plt.rcParams['font.family'] = 'Arial'
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+
+    ax1.plot(batch_indices, epoch_train_loss, label="Train Loss", color="#8A2BE2", marker="")
+    ax1.set_xlabel("Epoch", fontsize=14)
+    ax1.set_ylabel("Loss", color="black", fontsize=13)
+    ax1.tick_params(axis="y", labelcolor="black", labelsize=12)
+    ax1.set_xticks(range(num_epochs + 1))
+
+    ax2 = ax1.twinx()
+    ax2.plot(batch_indices, epoch_train_acc, label="Train Accuracy", color="#6495ED", marker="")
+    ax2.plot(epoch_indices, epoch_test_acc, label="Test Accuracy", color="#FF4500", marker="")
+    ax2.set_ylabel("Accuracy", color="black", fontsize=14)
+    ax2.tick_params(axis="y", labelcolor="black", labelsize=12)
+
+    plt.title("Training and Testing Loss and Accuracy Curves", fontsize=15)
+    fig.tight_layout()
+    fig.legend(ncol=3, bbox_to_anchor=(0.5, -0.06), loc='lower center',
+            edgecolor='w', fontsize=15)
+
+    plt.savefig(path, dpi=300,bbox_inches='tight')
+
+def train_model(net, train_iter, test_iter, num_epochs, lr, device, test=False, plot=False):
     # initialize weights
     net.apply(init_weights)
     # train on gpu?
@@ -52,19 +86,17 @@ def train_model(net, train_iter, test_iter, num_epochs, lr, device, plot=False):
     optimizer = get_optimizer(net, lr)
     # criterion
     loss = nn.CrossEntropyLoss()
-    if plot==True:
-        # For plotting data in animation
-        animator = d2l.Animator(xlabel='epoch', xlim=[1, num_epochs],
-                                legend=['train loss', 'train acc', 'test acc'])
-    timer, num_batches = d2l.Timer(), len(train_iter)
+    if plot == True:
+        train_record = [[], []]
+        if test == True:
+            test_record = []
 
     # start training
     for epoch in range(num_epochs):
-        metric = d2l.Accumulator(3)
+        metric = [0.0] * 3
         net.train()
         with tqdm(total=len(train_iter), desc=f"Epoch {epoch + 1}/{num_epochs}") as pbar:
             for i, (X, y) in enumerate(train_iter):
-                timer.start()
                 optimizer.zero_grad()
                 X, y = X.to(device), y.to(device)
                 y_hat = net(X)
@@ -72,81 +104,70 @@ def train_model(net, train_iter, test_iter, num_epochs, lr, device, plot=False):
                 l.backward()
                 optimizer.step()
                 with torch.no_grad():
-                    metric.add(l * X.shape[0], d2l.accuracy(y_hat, y), X.shape[0])
-                    timer.stop()
+                    metrics_list = [l * X.shape[0], cal_correct_num(y_hat, y), X.shape[0]]
+                    metric = [a + float(b) for a, b in zip(metric, metrics_list)]
                     train_l = metric[0] / metric[2]
                     train_acc = metric[1] / metric[2]
                     if plot == True:
-                        if (i + 1) % (num_batches // 5) == 0 or i == num_batches - 1:
-                            animator.add(epoch + (i + 1) / num_batches,
-                                        (train_l, train_acc, None))
-                # 更新进度条
+                        train_record[0].append(train_l)
+                        train_record[1].append(train_acc)
+                # update bar
                 pbar.set_postfix({"Loss": train_l, "Accuracy": train_acc})
                 pbar.update(1)
 
-        test_acc = evaluate_accuracy_gpu(net, test_iter)
-        if plot == True:
-            animator.add(epoch + 1, (None, None, test_acc))
-        print(f"Epoch {epoch + 1}/{num_epochs} -> Train Accuracy: {train_acc:.4f}, Train Loss: {train_l:.4f}, Test Accuracy: {test_acc:.4f}")
+        if test == True:
+            test_acc = cal_accuracy_gpu(net, test_iter)
+            print(f"Epoch {epoch + 1}/{num_epochs} -> Train Accuracy: {train_acc:.4f}, Train Loss: {train_l:.4f}, Test Accuracy: {test_acc:.4f}")
+        else:
+            print(f"Epoch {epoch + 1}/{num_epochs} -> Train Accuracy: {train_acc:.4f}, Train Loss: {train_l:.4f}")
         
-    print(f'Final loss {train_l:.3f}, Final Train acc {train_acc:.3f}, '
-          f'Final Test acc {test_acc:.3f}')
-    print(f'{metric[2] * num_epochs / timer.sum():.1f} examples/sec '
-          f'on {str(device)}')
-
-def main(train = None):
-    if train == 'Train':
-        # data preprocessing
-        trans = transforms.Compose([
-            transforms.ToTensor()
-        ])
-        mnist_train = torchvision.datasets.MNIST(
-            root=".data",
-            train=True,
-            transform=trans,
-            download=True)
-        mnist_test = torchvision.datasets.MNIST(
-            root=".data",
-            train=False,
-            transform=trans,
-            download=True)
+        if (plot == True) and (test == True):
+            test_record.append(test_acc)
         
-        batch_size = 128
-        train_iter = data.DataLoader(mnist_train, batch_size, shuffle=True)
-        test_iter = data.DataLoader(mnist_test, batch_size, shuffle=True)   
+    print(f'Final loss {train_l:.3f}, Final Train acc {train_acc:.3f}',end='')
+    print(f',Final Test acc {test_acc:.3f}') if test == True else None
+    if plot == True:
+        PATH = './img/Fig.jpg'
+        plot_curves(train_record[0], train_record[1], test_record, PATH)
+    
 
-        # define model and train
-        model = LeNet()
-        num_epochs = 5
-        train_model(net=model,
-                train_iter=train_iter,
-                test_iter=test_iter,
-                num_epochs=num_epochs,
-                lr=0.01,
-                device=try_gpu()
-                )
-        
-        # save model weights
-        model_weights = 'LeNet-MNIST-epoch5'
-        PATH = f'.checkpoints/MNIST_Classification/{model_weights}.pth'
-        save_model(model, PATH)
-    else:
-        test_model = LeNet()
-        MODEL_PATH = '.checkpoints/MNIST_Classification/LeNet-MNIST-epoch5.pth'
-        test_model.load_state_dict(torch.load(MODEL_PATH))
-        batch_size = 128
-        trans = transforms.Compose([
-            transforms.ToTensor()
-        ])
-        mnist_test = torchvision.datasets.MNIST(
-            root=".data",
-            train=False,
-            transform=trans,
-            download=True)
-        test_iter = data.DataLoader(mnist_test, batch_size, shuffle=True)
-        test_acc = evaluate_accuracy_gpu(test_model, test_iter)
-        print(test_acc)
+def main():
+    # data preprocessing
+    trans = transforms.Compose([
+        transforms.ToTensor()
+    ])
+    mnist_train = torchvision.datasets.MNIST(
+        root=".data",
+        train=True,
+        transform=trans,
+        download=True)
+    mnist_test = torchvision.datasets.MNIST(
+        root=".data",
+        train=False,
+        transform=trans,
+        download=True)
+    
+    batch_size = 64
+    train_iter = data.DataLoader(mnist_train, batch_size, shuffle=True)
+    test_iter = data.DataLoader(mnist_test, batch_size, shuffle=True)   
 
+    # define model and train
+    model = LeNet()
+    num_epochs = 50
+    train_model(net=model,
+            train_iter=train_iter,
+            test_iter=test_iter,
+            num_epochs=num_epochs,
+            lr=0.001,
+            device=try_gpu(),
+            plot=True,
+            test=True
+            )
+            
+    # save model weights
+    model_weights = 'LeNet-MNIST-epoch50'
+    PATH = f'.checkpoints/MNIST_Classification/{model_weights}.pth'
+    save_model(model, PATH)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
